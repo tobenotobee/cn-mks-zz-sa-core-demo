493ac5de3a952152e9ef24f39af930ec5dc58a0f
tobenotobee <31716865+tobenotobee@users.noreply.github.com>
2024-09-12 09:33:00 +0800

    first commit
    first commit

---
+++ src/main/java/a/b/c/d/controller/HelloController.java
@@ -0,0 +1,15 @@
+package a.b.c.d.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Spring boot";
+    }
+
+}
